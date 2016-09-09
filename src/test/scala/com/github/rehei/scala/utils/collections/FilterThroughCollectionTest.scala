4541963d414209a5e9cbcb1dc13ce171fc668d6f
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2016-09-09 09:06:31 +0200

    Implementing tests using Specs2 and mockito

--- src/test/scala/com/github/rehei/scala/utils/collections/FilterThroughCollectionTest.scala
+++ src/test/scala/com/github/rehei/scala/utils/collections/FilterThroughCollectionTest.scala
@@ -2,41 +2,51 @@ package com.github.rehei.scala.utils.collections
 
 import java.util.ArrayList
 
-import org.junit.Test
+import com.github.rehei.scala.utils.collections.model.Element
+import org.specs2._
+import org.specs2.mock.Mockito
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 import com.github.rehei.scala.utils.collections.model.Element
 
-class FilterThroughCollectionTest {
+@RunWith(classOf[JUnitRunner])
+class FilterThroughCollectionTest extends org.specs2.mutable.Specification with Mockito {
 
   val DEFAULT_CATEGORY = 2
 
-  @Test
-  def test() {
+  "A filtered list" >> {
+    "should only contain matching elements" >> {
 
-    val element1 = Element("1", DEFAULT_CATEGORY)
-    val element2 = Element("2", DEFAULT_CATEGORY)
-    val element3 = Element("3", DEFAULT_CATEGORY + 2)
+      val element1 = Element("1", DEFAULT_CATEGORY)
+      val element2 = Element("2", DEFAULT_CATEGORY)
+      val element3 = Element("3", DEFAULT_CATEGORY + 2)
 
-    val base = new ArrayList[Element]()
-    base.add(element1)
-    base.add(element2)
-    base.add(element3)
+      val base = new ArrayList[Element]()
+      base.add(element1)
+      base.add(element2)
+      base.add(element3)
 
-    val elementMatchingCategory = Element("matching category", DEFAULT_CATEGORY)
-    val elementNotMatchingCategory = Element("not matching category", DEFAULT_CATEGORY + 1)
+      val elementMatchingCategory = Element("matching category", DEFAULT_CATEGORY)
+      val elementNotMatchingCategory = Element("not matching category", DEFAULT_CATEGORY + 1)
 
-    val list = CollectionFactory(base).filterThrough(_.category == DEFAULT_CATEGORY)
+      val list = CollectionFactory(base).filterThrough(_.category == DEFAULT_CATEGORY)
 
-    list.add(elementMatchingCategory)
-    list.add(elementNotMatchingCategory)
+      list.add(elementMatchingCategory)
+      list.add(elementNotMatchingCategory)
 
-    assert(list.size() == 3)
-    assert(list.contains(element1) == true)
-    assert(list.contains(element2) == true)
-    assert(list.contains(element3) == false)
-    assert(list.contains(elementMatchingCategory) == true)
-    assert(list.contains(elementNotMatchingCategory) == false)
+      assert(list.size() == 3)
+      assert(list.contains(element1) == true)
+      assert(list.contains(element2) == true)
+      assert(list.contains(element3) == false)
+      assert(list.contains(elementMatchingCategory) == true)
+      assert(list.contains(elementNotMatchingCategory) == false)
 
+      there was none
+    }
   }
+
 
 }
--- src/test/scala/com/github/rehei/scala/utils/collections/ModifyOnInsertCollectionTest.scala
+++ src/test/scala/com/github/rehei/scala/utils/collections/ModifyOnInsertCollectionTest.scala
@@ -2,42 +2,48 @@ package com.github.rehei.scala.utils.collections
 
 import java.util.ArrayList
 
-import scala.collection.JavaConverters.asJavaCollectionConverter
-
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
-import org.junit.Test
-
 import com.github.rehei.scala.utils.collections.model.Element
+import org.specs2._
+import org.specs2.mock.Mockito
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import org.junit.Assert._
 
-class ModifyOnInsertCollectionTest {
+@RunWith(classOf[JUnitRunner])
+class ModifyOnInsertCollectionTest extends org.specs2.mutable.Specification with Mockito {
 
   val DEFAULT_CATEGORY = 5
 
-  @Test
-  def test() {
-    val elementA = Element("a", DEFAULT_CATEGORY + 5)
-    val elementB = Element("b", DEFAULT_CATEGORY + 6)
-    val elementC = Element("c", DEFAULT_CATEGORY + 7)
-    val elementD = Element("d", DEFAULT_CATEGORY + 8)
+  "A ModifyOnInsertList list" >> {
+    "should update elements on insert" >> {
 
-    val elementsCandD = List(elementC, elementD).asJavaCollection
+      val elementA = Element("a", DEFAULT_CATEGORY + 5)
+      val elementB = Element("b", DEFAULT_CATEGORY + 6)
+      val elementC = Element("c", DEFAULT_CATEGORY + 7)
+      val elementD = Element("d", DEFAULT_CATEGORY + 8)
 
-    val base = new ArrayList[Element]()
-    val result = CollectionFactory(base).modifyOnInsert(_.category = DEFAULT_CATEGORY)
-    result.add(elementA)
-    result.add(elementB)
-    result.addAll(elementsCandD)
+      val elementsCandD = List(elementC, elementD).asJavaCollection
 
-    assertTrue(elementA.category == DEFAULT_CATEGORY)
-    assertTrue(elementB.category == DEFAULT_CATEGORY)
-    assertTrue(elementC.category == DEFAULT_CATEGORY)
-    assertTrue(elementD.category == DEFAULT_CATEGORY)
-    assertTrue(result.contains(elementA))
-    assertTrue(result.contains(elementB))
-    assertTrue(result.containsAll(elementsCandD))
-    assertTrue(result.size() == 4)
-    assertFalse(result.isEmpty())
+      val base = new ArrayList[Element]()
+      val result = CollectionFactory(base).modifyOnInsert(_.category = DEFAULT_CATEGORY)
+      result.add(elementA)
+      result.add(elementB)
+      result.addAll(elementsCandD)
+
+      assertTrue(elementA.category == DEFAULT_CATEGORY)
+      assertTrue(elementB.category == DEFAULT_CATEGORY)
+      assertTrue(elementC.category == DEFAULT_CATEGORY)
+      assertTrue(elementD.category == DEFAULT_CATEGORY)
+      assertTrue(result.contains(elementA))
+      assertTrue(result.contains(elementB))
+      assertTrue(result.containsAll(elementsCandD))
+      assertTrue(result.size() == 4)
+      assertFalse(result.isEmpty())
+
+      there was none
+    }
   }
 
 }
